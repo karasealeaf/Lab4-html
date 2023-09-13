53d4dadcacfcd1cb57e5bc5fa5c82315ed393e66
kara sealeaf <kara.sealeaf13@gmail.com>
2023-09-13 15:31:19 +0100

    CSS added and JS

---
+++ app.js
@@ -0,0 +1,11 @@
+//variables – bucket for information
+let myName = "Kara";
+let yourName = "Tim";
+
+Console.log (myName,yourName);
+
+let myAge= 33;
+// data types
+let string = "anything within quotes";
+let number = 10;
+let Boolean = true; // or false
