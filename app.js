c212fed346555d4813eb09e35449aa259901abe5
kara sealeaf <kara.sealeaf13@gmail.com>
2023-09-15 12:02:11 +0100

    done

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ function getRating () {
     let rating = prompt ('Rate my website between 1-5', '5');
 
     for (let i = 0; i < rating; i++) {
-        document.write ("<img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.stickerscape.co.uk%2Fwp-content%2Fuploads%2F003305CP-W.jpg&tbnid=t9KtomVoLZlTgM&vet=12ahUKEwiDu8-Tq6yBAxXYsCcCHQsNCz8QMygAegQIARBv..i&imgrefurl=https%3A%2F%2Fwww.stickerscape.co.uk%2Fproduct%2Fpink-giant-star-wall-sticker%2F&docid=TfHCs3c0CejwnM&w=900&h=900&q=pink%20star&ved=2ahUKEwiDu8-Tq6yBAxXYsCcCHQsNCz8QMygAegQIARBv'/>")
+        document.write ("<img class='star' src='./images/star.png'/>")
     }
+}
+console.log("Hey");
+
+function greet () {
+    let username = prompt ("What is your name?");
+    document.write("Hello " + username);
+}
+
+function askTime() {
+    let time = prompt("What is the time?");
+    let message;
+
+   if (time <= 11) {
+    message = "Good Morning, how are you?";
+   } else if (time <= 17) {
+    message = "Good Afternoon, how are you?";
+   } else if (time < 23) {
+    message = "Good Evening, how are you?";
+   } document.write(message);  
+
 }
