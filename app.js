4352a85ffe9ae6c6ae7a75fee55e5cf29d143977
kara sealeaf <kara.sealeaf13@gmail.com>
2023-09-15 10:52:28 +0100

    Rating

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-console.log("Hey");
+console.log("Welcome!")
 
-function greet () {
-    let username = prompt ("What is your name?");
-    document.write("Hello " + username);
-}
+function getRating () {
+    let rating = prompt ('Rate my website between 1-5', '5');
 
-function askTime() {
-    let time = prompt("What is the time?");
-    let message;
-
-   if (time <= 11) {
-    message = "Good Morning!";
-   } else if (time <= 17) {
-    message = "Good Afternoon!";
-   } else if (time < 24); {
-    message = "Good Evening!";
-   } document.write(message);  
-
+    for (let i = 0; i < rating; i++) {
+        document.write ("<img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.stickerscape.co.uk%2Fwp-content%2Fuploads%2F003305CP-W.jpg&tbnid=t9KtomVoLZlTgM&vet=12ahUKEwiDu8-Tq6yBAxXYsCcCHQsNCz8QMygAegQIARBv..i&imgrefurl=https%3A%2F%2Fwww.stickerscape.co.uk%2Fproduct%2Fpink-giant-star-wall-sticker%2F&docid=TfHCs3c0CejwnM&w=900&h=900&q=pink%20star&ved=2ahUKEwiDu8-Tq6yBAxXYsCcCHQsNCz8QMygAegQIARBv'/>")
+    }
 }
